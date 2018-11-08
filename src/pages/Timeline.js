e8add71bacd9988b121cb366946ba58166e3ea80
03219759360 <tonnydallios@gmail.com>
2018-11-08 12:57:37 -0300

    Adicionando novo tweets via react js

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+
 import twitterLogo from '../twitter.svg';
 import './Timeline.css';
+
 
 export default class Timeline extends Component {
   state = {
     newTweet: ''
   }
 
-  handleNewTweet = e => {
+  handleNewTweet = async e => {
     if (e.keyCode !== 13) return;
 
     const content = this.state.newTweet;
     const author = localStorage.getItem("@GoTwitter:username");
 
-    console.log(content, author)
+    await api.post('tweets', { content, author })
+
+    this.setState({ newTweet: '' })
   }
 
   handleInputChange = e => {
